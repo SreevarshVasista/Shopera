fc51bd2c5f66bdf10e621c94bdb9fb6dc6402f90
Sreevarsh Vasista <vasistasreevarsh1@gmail.com>
2021-06-25 10:28:07 +0530

    firebase added

---
+++ app/src/main/java/com/example/sreevarsh/shopera/ListDetails.java
@@ -0,0 +1,16 @@
+package com.example.sreevarsh.shopera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_details);
+
+
+    }
+}
